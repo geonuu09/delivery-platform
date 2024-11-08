4625c2703154510f39c7d917b315b551a6ea6d92
LeeGeonu <rjsdndl909@gmail.com>
2024-11-08 14:23:59 +0900

    user Entity 생성

---
+++ src/main/java/com/example/delivery/user/entity/User.java
@@ -0,0 +1,47 @@
+package com.example.delivery.user.entity;
+
+import com.example.delivery.common.entity.Timestamped;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User extends Timestamped {
+    @Id
+    private String userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRoleEnum role;
+
+    @Column(nullable = false)
+    private String address1;
+
+    @Column(nullable = false)
+    private String address2;
+
+    @Column(nullable = false)
+    private String phoneNum;
+
+
+}
+
