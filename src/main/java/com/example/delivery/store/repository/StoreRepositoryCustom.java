318a4f74bb27370f36b876fd7826da5237c1c11c
LeeGeonu <rjsdndl909@gmail.com>
2024-11-16 15:23:51 +0900

    refactor : 코드 파일 수정

--- src/main/java/com/example/delivery/store/repository/StoreRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.example.delivery.store.repository;
-
-import com.example.delivery.store.entity.Store;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface StoreRepositoryCustom {
-    //Page<Store> findStoresByKeyword(String keyword, Pageable pageable);
-}
